66e1a1d2a81890a4136407c50bd42405e4b46780
Raul Rodriguez <raul@raulr.net>
2016-02-18 20:04:27 +0100

    Generate locale to support filenames with UTF-8 chars

--- files/install.sh
+++ files/install.sh
@@ -6,6 +6,13 @@ apk update
 apk add bash wget ca-certificates tar expect findutils coreutils procps
 apk add cpio --update-cache --repository http://dl-3.alpinelinux.org/alpine/edge/testing/
 
+# Generate en_US.UTF-8 locale
+wget https://github.com/andyshinn/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-i18n-${GLIBC_VERSION}.apk
+apk --allow-untrusted add glibc-i18n-${GLIBC_VERSION}.apk
+/usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8
+apk del glibc-i18n
+rm glibc-i18n-${GLIBC_VERSION}.apk
+
 mkdir /tmp/crashplan
 
 wget -O- http://download.code42.com/installs/linux/install/CrashPlan/CrashPlan_${CRASHPLAN_VERSION}_Linux.tgz \
